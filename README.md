588ef65191b23786a2af73947967f19294faebdc
yusanshi <yulei2018@gmail.com>
2020-03-03 21:02:02 +0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# Handwriting Generator
+
+Handwriting text generator. https://yusanshi.com/handwriting/
+
+![](https://img.yusanshi.com/upload/20200303205851680031.png)
+
+![](https://img.yusanshi.com/upload/20200303205923498308.png)
+
+## Get started
+
+Clone the project. Visit https://www.foundertype.com/ , download `FZZJ-WXCZTJW.TTF`（方正手迹-王潇趁早体 简）to `./fonts`.
+
+## TODO
+
+- [ ] Custom letter space
+- [ ] Support more papers
+- [ ] Support more fonts
+- [ ] Blur effect
+
+## LICENSE
+
+MIT License
+
+## Credits
+
+<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- app.js
+++ app.js
@@ -15,7 +15,8 @@ window.onload = () => {
         e.target["fontScale"].value,
         e.target["paper"].value,
         e.target["letterSpace"].value,
-        e.target["leftBias"].value
+        e.target["leftBias"].value,
+        e.target["paperSlope"].value
       );
     }
   });
@@ -23,7 +24,15 @@ window.onload = () => {
   document.querySelector("#downloadPDF").addEventListener("click", downloadPDF);
 };
 
-async function generate(text, font, fontScale, paper, letterSpace, leftBias) {
+async function generate(
+  text,
+  font,
+  fontScale,
+  paper,
+  letterSpace,
+  leftBias,
+  paperSlope
+) {
   document.querySelector("#downloadPDF").setAttribute("disabled", "disabled");
   document.querySelector("#preview").innerHTML = "";
   currentConfig = paperConfig[paper];
@@ -32,8 +41,23 @@ async function generate(text, font, fontScale, paper, letterSpace, leftBias) {
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
 
-    canvas.height = currentConfig["height"] * 10;
     canvas.width = currentConfig["width"] * 10;
+    canvas.height = currentConfig["height"] * 10;
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    const radian = ((Math.random() - 0.5) * 2 * paperSlope * Math.PI) / 180;
+    ctx.rotate(radian);
+    // How to get scale factor such that after rotate, no blank area exist
+    // wolframalpha: Solve[{y - b/2*cos(θ) == (x - b/2*sin(θ))*(-tan(θ)), y==b/a*x},{x,y}]
+    // get x than factor equals to a/2/x, i.e., factor = (tan(θ)/k + 1)cos(θ)
+    // Also, abs() is needed i.e. it works for both clockwise and anti-clockwise rotatation
+    const radianAbs = Math.abs(radian);
+    let k = canvas.height / canvas.width;
+    if (k > 1) {
+      k = 1 / k;
+    }
+    const factor = (Math.tan(radianAbs) / k + 1) * Math.cos(radianAbs);
+    ctx.scale(factor, factor);
+    ctx.translate(-canvas.width / 2, -canvas.height / 2);
 
     // render background
     const loadingTask = pdfjsLib.getDocument(`./papers/${paper}.pdf`);
@@ -60,7 +84,7 @@ async function generate(text, font, fontScale, paper, letterSpace, leftBias) {
     if (!document.fonts.check(fontRep)) {
       const fontLoaded = await new FontFace(
         font,
-        `url(fonts/${font}.ttf)`
+        `url(fonts/${font}.TTF)`
       ).load();
       document.fonts.add(fontLoaded);
     }
@@ -98,13 +122,13 @@ async function generate(text, font, fontScale, paper, letterSpace, leftBias) {
 
       ctx.fillText(
         text.slice(consumed, consumed + lineConsumed),
-        (currentConfig["start"]["x"] + (Math.random() - 0.5) * 2 * leftBias) *
-          canvas.width,
+        (currentConfig["start"]["x"] + Math.random() * leftBias) * canvas.width,
         (currentConfig["start"]["y"] + currentLine * line_height) *
           canvas.height
       );
       consumed += lineConsumed;
     }
+
     document.querySelector("#preview").appendChild(canvas);
     text = text.slice(consumed, text.length);
   }
